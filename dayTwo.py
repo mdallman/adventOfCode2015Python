a9ae47dba6bea4be7e76bfd92f3b61d312d87b5d
mdallman <52248222+mdallman@users.noreply.github.com>
2020-03-21 00:49:12 +0000

    Added funcationality to calculate ribbon.

--- dayTwo.py
+++ dayTwo.py
@@ -7,7 +7,6 @@
 #Given a present with sides l, w, h
 #Part 1 - Find the amount of wrapping paper needed.
 
-#calculates the amount of wrapping paper per present.
 def calculateWrapping(sizes):
     #This function calculates the armount of paper needed for one present.
     #Get the length of the sides from string
@@ -36,23 +35,37 @@ def calculateWrapping(sizes):
     paperNeeded = (2 * lw) + (2 * lh) + (2 * wh) + smallestSide
     return paperNeeded
 
+
+def calcRibbon(sizes):
+    #Gets the length, height and width.
+    sides = sizes.split('x')
+    l = int(sides[0])
+    w = int(sides[1])
+    h = int(sides[2])
+
+    faceLW = 2 * (l+w)
+    faceLH = 2 * (l+h)
+    faceWH = 2 * (w+h)
+    volume = l * w * h
+
+    smallPeri = faceLW
+    if smallPeri > faceLH:
+        smallPeri = faceLH
+
+    if smallPeri > faceWH:
+        smallPeri = faceWH
+
+    ribbonRequired = volume + smallPeri
+    return ribbonRequired
+    
+
 totalPaper = 0
+totalRibbon = 0
 f = open('dayTwoInput.txt   ',mode='r')
 for x in f.readlines():
     totalPaper += calculateWrapping(x)
+    totalRibbon += calcRibbon(x)
     
-<<<<<<< HEAD
-print(totalPaper)
-=======
-#takes the list and iterates using calculateWrapping
-def allWrapping():
-    totalPaper = 0
-    #f = open("dayTwoTest.txt", "r") #uses the test file
-    f = open("dayTwoInput.txt", "r") #The project input file
-    for line in f:
-        totalPaper += calculateWrapping(line)
-
-    return totalPaper
-
-print("The total amount of wrapping paper needed is:", allWrapping())
->>>>>>> caf980b28bd6828499e673d7598fca503d104d48
+    
+print('Total amount of wrapping paper required:',totalPaper)
+print('Total amount of ribbon required:',totalRibbon)
