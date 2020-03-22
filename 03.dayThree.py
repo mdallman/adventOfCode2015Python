dbb56e53dabafaccedf5ea3a6a703530ffe054ae
M.Dallman <matthew.dallman@ntlworld.com>
2020-03-22 17:15:02 +0000

    Movement Function created dayThree

--- 03.dayThree.py
+++ 03.dayThree.py
@@ -3,3 +3,21 @@
 #  By Matt Dallman     #
 ########################
 
+#Initialise Variables
+xSanta = 0
+ySanta = 0
+santaPosition = [xSanta,ySanta]
+
+
+def movement(direction):
+    global xSanta
+    global ySanta
+
+    if direction == 'v':
+        ySanta -= 1
+    elif direction == '^':
+        ySanta += 1
+    elif direction == '>':
+        xSanta += 1
+    elif direction == '<':
+        xSanta -= 1
