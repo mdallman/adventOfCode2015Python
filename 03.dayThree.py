7a1a6cd41a8991fea5fa42ec73701b7615790517
M.Dallman <matthew.dallman@ntlworld.com>
2020-03-22 17:34:19 +0000

    Update 03.dayThree.py
    Started to write checkHouse function and the return the number of houses santa visited.

--- 03.dayThree.py
+++ 03.dayThree.py
@@ -7,12 +7,16 @@
 xSanta = 0
 ySanta = 0
 santaPosition = [xSanta,ySanta]
-
+santaHouse = []
+housesVisited = 0
 
 def movement(direction):
+    #Declare variables
     global xSanta
     global ySanta
+    global santaPosition
 
+    #Reads instructions and moves santa based on them
     if direction == 'v':
         ySanta -= 1
     elif direction == '^':
@@ -21,3 +25,17 @@ def movement(direction):
         xSanta += 1
     elif direction == '<':
         xSanta -= 1
+
+    santaPosition = [xSanta,ySanta]
+
+def checkHouse(position):
+    global santaPosition
+    global santaHouse
+    global housesVisited
+
+    #check if santaPosition exists in housesVisited
+    #How to check, binary search/bubble search? - which one to use
+        #if it doesn't exist add it into housesVisited
+
+#prints how many houses Santa has visited.
+print('Santa has been to',len(santaHouse),'houses.')
