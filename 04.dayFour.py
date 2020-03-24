73c6e29308380b1355043b7551d415cb5d450c5d
mdallman <matthew.dallman@ntlworld.com>
2020-03-24 16:05:42 +0000

    Update 04.dayFour.py
    Updated for 6 zeros, without affecting 5 zero functionality.

--- 04.dayFour.py
+++ 04.dayFour.py
@@ -10,6 +10,8 @@ secretKey = 'yzbqklnj'
 testSecretKey = 'abcdef'
 testSecretKeyTwo = 'pqrstuv'
 leadingNumber = 0
+fiveZeros = False
+sixZeros = False
 
 def tryHash(key,number):
     #combines the two strings hashes them and returns the hex.
@@ -19,8 +21,29 @@ def tryHash(key,number):
     hexKey = hashedKey.hexdigest()
     return hexKey
 
-def checkZeros(md5Hash):
+def checkFiveZeros(md5Hash):
     if md5Hash[0:5] == '00000':
         return True
     else:
         return False
+
+def checkSixZeros(md5Hash):
+    if md5Hash[0:6] == '000000':
+        return True
+    else:
+        return False
+
+while fiveZeros == False:
+    leadingNumber += 1
+    activeKey = tryHash(secretKey,leadingNumber)
+    fiveZeros = checkFiveZeros(activeKey)
+
+print('The first positive number that gives 5 leading zeros for',secretKey,'is',leadingNumber)
+
+
+while sixZeros == False:
+    leadingNumber += 1
+    activeKey = tryHash(secretKey,leadingNumber)
+    sixZeros = checkSixZeros(activeKey)
+
+print('The first positive number that gives 6 leading zeros for',secretKey,'is',leadingNumber)
