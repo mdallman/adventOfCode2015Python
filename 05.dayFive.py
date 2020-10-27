a297aed9a47d98ec971a65ae4ac5639302867fb9
mdallman <matthew.dallman@ntlworld.com>
2020-10-27 12:35:11 +0000

    Update 05.dayFive.py

--- 05.dayFive.py
+++ 05.dayFive.py
@@ -2,3 +2,13 @@
 # Advent of Code 2015 Day 5 #
 #     By Matt Dallman       #
 #############################
+
+
+#TODO Define the Check if Nice String Function
+
+#TODO Define check if at least three vowels
+
+#TODO Define check for at least one set of  double letters
+
+#TODO Define Check for strings 'ab' 'cd' 'pq' 'xy'
+
